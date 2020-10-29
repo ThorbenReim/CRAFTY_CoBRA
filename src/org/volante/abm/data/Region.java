e8b8db67714fc54426d1e96fc281b161b80b0718
alanbseo <pompoco78@gmail.com>
2020-10-29 23:07:43 +0100

    Auto stash before merge of "CRAFTY2" and "origin/CRAFTY2"

--- src/org/volante/abm/data/Region.java
+++ src/org/volante/abm/data/Region.java
@@ -240,7 +240,7 @@ public class Region implements Regions, PreTickAction {
 		
  
 		// gt-opengis-9.0.jar must be included before geoapi-20050403.jar!
-		// Otherwise this method throws an uncatchable error 
+		// Otherwise this method throws an uncatchable error.
 		
 		if (this.geography == null) {
 			
@@ -248,8 +248,8 @@ public class Region implements Regions, PreTickAction {
 			// Causes the CRS factory to apply (longitude, latitude) order of
 			// axis:
 			// TODO
-			// System.setProperty(GeoTools.FORCE_LONGITUDE_FIRST_AXIS_ORDER,
-			// "true");
+//			 System.setProperty(GeoTools.FORCE_LONGITUDE_FIRST_AXIS_ORDER,
+//			 "true");
 			GeographyParameters<Object> geoParams = new GeographyParameters<Object>();
 			geoParams.setCrs((String) PmParameterManager
 					.getParameter(GeoPa.CRS));
@@ -268,8 +268,8 @@ public class Region implements Regions, PreTickAction {
 			// LOGGING ->
 			} catch (Exception e) { 
 				
-				logger.info("Geography is null and failed to generate one");
-				e.printStackTrace();
+				logger.error("Geography is null and failed to generate one");
+//				e.printStackTrace();
 				
 			}
 			
