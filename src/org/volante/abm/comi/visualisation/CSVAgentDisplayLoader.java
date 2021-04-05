162d275e58d2037559cf3a7a7e8084de9f0f7b9c
alanbseo <pompoco78@gmail.com>
2021-04-06 00:48:51 +0200

    Read agent color code from an external csv file
    Has to be specified in the display section of a scenario file. E.g., 
     
        <display class="org.volante.abm.comi.visualisation.CSVAgentDisplayLoader" file="csv/AgentColors.csv" indexed="true" title="AgentTypes"/>

---
+++ src/org/volante/abm/comi/visualisation/CSVAgentDisplayLoader.java
@@ -0,0 +1,77 @@
+/**
+ * This file is part of
+ * 
+ * CRAFTY - Competition for Resources between Agent Functional TYpes
+ *
+ * Copyright (C) 2021
+ * 
+ * CRAFTY is free software: You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * CRAFTY is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.volante.abm.comi.visualisation;
+
+import org.simpleframework.xml.Attribute;
+import org.volante.abm.data.ModelData;
+import org.volante.abm.data.Regions;
+import org.volante.abm.schedule.RunInfo;
+import org.volante.abm.visualisation.AgentTypeDisplay;
+import com.csvreader.CsvReader;
+import java.awt.Color;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+
+// Read agent colors from a csv file (Apr21 by ABS)
+
+public class CSVAgentDisplayLoader extends AgentTypeDisplay {
+	/**
+	 * 
+	 */
+	//	private static final long serialVersionUID = -8136199562481069547L; // @TODO change the ID?
+
+	/**
+	 * Logger
+	 */
+	static private Logger logger = Logger.getLogger(CSVAgentDisplayLoader.class);
+
+
+	@Attribute(name = "file", required= true)
+	String	file		= "";
+	//	@Attribute(name = "indexed", required = false)
+	//	boolean	indexed		= true;
+	@Attribute(name = "nameColumn", required = false)
+	String	nameColumn	= "Name";
+	@Attribute(name = "colorColumn", required = false)
+	String	colorColumn	= "Color";
+
+	public CSVAgentDisplayLoader() {
+		logger.info("Read agent colors from a csv file:" + file);
+	}
+
+	@Override
+	public void initialise(ModelData data, RunInfo info, Regions region) throws Exception {
+		super.initialise(data, info, region);
+		CsvReader reader = info.getPersister().getCSVReader(file, null);
+
+		while (reader.readRecord()) {
+			String aft = reader.get(nameColumn);
+			logger.info(aft + ": " + reader.get(colorColumn));
+			addAgent(aft, Color.decode(reader.get(colorColumn))); //conver #hexcode to java.awt.Color
+		}
+
+	}
+}
+
+
--- src/org/volante/abm/comi/visualisation/UKAgents.java
+++ src/org/volante/abm/comi/visualisation/UKAgents.java
@@ -13,26 +13,27 @@ public class UKAgents extends AgentTypeDisplay
 
 	public UKAgents()
 	{
-		addAgent("NWCons", Color.magenta.brighter());
-		addAgent("Bioenergy", Color.cyan.darker());
-		addAgent("EA", Color.orange.darker());
-		addAgent("EP", new Color(251,154,153));
-		addAgent("IAfodder", Color.red.brighter());
-		addAgent("IAfood", Color.blue.darker());
-		addAgent("PNNB", Color.yellow.brighter() );
-		addAgent("MW", Color.green.brighter());		
-		addAgent("AF", new Color(31,120,180)); // #1f78b4
-		addAgent("PNB", new Color(255,127,0) );	
-		addAgent("PNC", Color.green.darker());	
-		addAgent("PNNC", Color.pink.darker());
-		addAgent("IP",new Color(178,223,138) );// #b2df8a
-		addAgent("SusAr", Color.cyan.brighter());
-		addAgent("VEP", new Color(51,160,44)); //	#33a02c
-		addAgent("Urban", Color.gray.darker() ); // #6a3d9a	
-		addAgent("Lazy FR", Color.white.brighter());	
+		addAgent("NWCons", Color.magenta.brighter());//FF00FF
+		addAgent("Bioenergy", Color.cyan.darker()); //00B2B2
+		addAgent("EA", Color.orange.darker()); 		//B28C00
+		addAgent("EP", new Color(251,154,153));     //FB9A99
+		addAgent("IAfodder", Color.red.brighter()); //FF0000
+		addAgent("IAfood", Color.blue.darker());    //0000b2
+		addAgent("PNNB", Color.yellow.brighter() ); //FFFF00
+		addAgent("MW", Color.green.brighter());		//00FF00
+		addAgent("AF", new Color(31,120,180)); 		//1F78B4
+		addAgent("PNB", new Color(255,127,0) );		//ff7f00
+		addAgent("PNC", Color.green.darker());		//00b200
+		addAgent("PNNC", Color.pink.darker());		//b27a7a
+		addAgent("IP",new Color(178,223,138) );		//b2df8a
+		addAgent("SusAr", Color.cyan.brighter());	//00ffff
+		addAgent("VEP", new Color(51,160,44)); 		//33a02c
+		addAgent("Urban", Color.gray.darker() ); 	//595959	
+		addAgent("Lazy FR", Color.white.brighter());//ffffff
 	}
 }
 
+//String.format("%06x", 0xFFFFFF & Color.BLUE.getRGB())
 
 // urban = red, cropland intensity classes from yellow to orange,
 // cropland is yellow/orange/brown, 
