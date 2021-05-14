24ea5ffe10a42cf6e01263c2a13cbce0e2447c92
alanbseo <pompoco78@gmail.com>
2021-05-14 23:27:30 +0200

    Parallelised NeighbourhoodNetwork institution
    using parallel stream

--- src/org/volante/abm/institutions/NeighbourhoodNetworkInstitution.java
+++ src/org/volante/abm/institutions/NeighbourhoodNetworkInstitution.java
@@ -3,34 +3,21 @@
  */
 package org.volante.abm.institutions;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collection;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.simpleframework.xml.Element;
 import org.volante.abm.agent.Agent;
 import org.volante.abm.agent.fr.FunctionalRole;
-import org.volante.abm.data.Capital;
 import org.volante.abm.data.Cell;
 import org.volante.abm.data.ModelData;
 import org.volante.abm.data.Region;
 import org.volante.abm.data.Service;
-import org.volante.abm.decision.pa.CompetitivenessAdjustingPa;
 import org.volante.abm.example.AgentPropertyIds;
-import org.volante.abm.example.RegionalDemandModel;
-import org.volante.abm.institutions.pa.RegionalSubsidyPa.RegionalSubsidyPaPreferences;
-import org.volante.abm.schedule.PrePreTickAction;
 import org.volante.abm.schedule.RunInfo;
-import org.volante.abm.update.CSVCapitalUpdater;
-
-import com.moseph.modelutils.curve.Curve;
-import com.moseph.modelutils.fastdata.DoubleMap;
 import com.moseph.modelutils.fastdata.UnmodifiableNumberMap;
-
-import de.cesr.lara.components.LaraPreference;
-import de.cesr.lara.components.decision.LaraDecisionConfiguration;
+import java.util.stream.Collectors;
 
 
 /**
@@ -49,14 +36,6 @@ public class NeighbourhoodNetworkInstitution extends AbstractInstitution {
 	static private Logger logger = Logger
 			.getLogger(NeighbourhoodNetworkInstitution.class);
 
-
-	/**
-	 * Name of CSV file that contains per-tick capital adjustment factors
-	 * relative to the base capitals.
-	 */
-	//	@Element(required = false)
-	//	String capitalAdjustmentsCSV = null;
-
 	/**
 	 * Neighbourhood size in pixel 
 	 */
@@ -71,28 +50,53 @@ public class NeighbourhoodNetworkInstitution extends AbstractInstitution {
 	private float effectFactor = (float) 0.1;
 
 
- 
 
 
 	/**
-	 * Update aft density 
+	 * Update AFT density 
 	 * Called at the start of each tick to allow this institution to perform any
 	 * internal updates necessary.
 	 */
 	@Override
 	public void update() { 
-		
+
 		// <- LOGGING
 		logger.info("Update neighbourhood density " + this);
 		// LOGGING ->
 
-		// @TODO parallelise
-		for (Cell c : this.region.getAllCells()) {
+		// For-loop version
+		//		for (Cell c : this.region.getAllCells()) {
+		//
+		//			if (c.getOwner() != Agent.NOT_MANAGED && c.getOwner() != null ) {
+		//
+		//				int cId = c.getOwnersFrSerialID();
+		//				var sameNeighbours = 0;
+		//
+		//				Set<Cell> neighbours = region.getAdjacentCells(c, neighbourhoodRadius);
+		//
+		//				// Count neighbouring cells under the same management
+		//
+		//				for (Cell n : neighbours) {
+		//					if (n.getOwnersFrSerialID() == cId) {
+		//						sameNeighbours++;
+		//					}
+		//				}
+		//				// Sum of the AFT density multiplied by effectOnCapitalFactor
+		//				float snStrength = (float) sameNeighbours / (float) neighbours.size() * effectFactor;
+		// 
+		//				c.setObjectProperty(AgentPropertyIds.SN_STRENGTH, (double) snStrength);
+		//
+		//			}
+		//		}
+
+		// parallelised stream
+
+		((Collection<Cell>) this.region.getAllCells()).parallelStream().forEach(c -> {
 
 			if (c.getOwner() != Agent.NOT_MANAGED && c.getOwner() != null ) {
 
 				int cId = c.getOwnersFrSerialID();
-				int sameNeighbours = 0;
+				var sameNeighbours = 0;
 
 				Set<Cell> neighbours = region.getAdjacentCells(c, neighbourhoodRadius);
 
@@ -105,33 +109,31 @@ public class NeighbourhoodNetworkInstitution extends AbstractInstitution {
 				}
 				// Sum of the AFT density multiplied by effectOnCapitalFactor
 				float snStrength = (float) sameNeighbours / (float) neighbours.size() * effectFactor;
-				// logger.trace("snStrength = density * effectFactor ="+snStrength);
 
 				c.setObjectProperty(AgentPropertyIds.SN_STRENGTH, (double) snStrength);
 
-
 			}
-		}
+		});		
+
+
 	}
 
 
 	@Override
 	public double adjustCompetitiveness(FunctionalRole fRole, Cell c, UnmodifiableNumberMap<Service> provision,
 			double competitiveness) {
-		
+
 		double adjustFactor = 1.0;
 
 		if (c.getOwner() != Agent.NOT_MANAGED && c.getOwner() != null ) {
 
-			//		try {
-			adjustFactor += (double) c.getObjectProperty(AgentPropertyIds.SN_STRENGTH) ; 
-			//		} catch (IllegalStateException ise) { 
-			//			ise.printStackTrace();
-			//		}
+			adjustFactor += (double) c.getObjectProperty(AgentPropertyIds.SN_STRENGTH);
+
 		}			
 
 		double adjustedComp = competitiveness * adjustFactor;
 		//logger.trace("comp adjusted from " + competitiveness + " to " + adjustedComp);
+
 
 		return adjustedComp;
 	}
@@ -144,52 +146,27 @@ public class NeighbourhoodNetworkInstitution extends AbstractInstitution {
 			throws Exception {
 		super.initialise(data, info, extent);
 
+		// Inform the model to adjust competitiveness from now on
 		extent.setHasCompetitivenessAdjustingInstitution();
 
-		// initialise sn strength
-		for (Cell c : this.region.getAllCells()) {
-
+		// initialise SN strength (otherwise later threw IllegalStateException) 		
+		//	for (Cell c : this.region.getAllCells()) {
+		//		c.setObjectProperty(AgentPropertyIds.SN_STRENGTH, 0.0);
+		//	}
+		
+		// parallelised
+		((Collection<Cell>) this.region.getAllCells()).parallelStream().forEach(c -> {
 			c.setObjectProperty(AgentPropertyIds.SN_STRENGTH, 0.0);
-
-		}
+		});
 
 		// <- LOGGING
 		logger.info("Initialise " + this);
 		// LOGGING ->
 
-		//extent.setRequiresEffectiveCapitalData();
-		//		if (capitalAdjustmentsCSV != null) {
-		//			loadCapitalFactorCurves();
-		//		}
 	}
 
 
-	//	/**
-	//	 * @throws IOException
-	//	 */
-	//	void loadCapitalFactorCurves() throws IOException {
-	//		// <- LOGGING
-	//		logger.info("Load capital adjustment factors from "
-	//				+ capitalAdjustmentsCSV);
-	//		// LOGGING ->
-	//
-	//		try {
-	//			Map<String, LinearInterpolator> curves = rInfo.getPersister()
-	//					.csvVerticalToCurves(capitalAdjustmentsCSV, tickCol,
-	//							modelData.capitals.names(), this.region.getPersisterContextExtra());
-	//			for (Capital c : modelData.capitals) {
-	//				if (curves.containsKey(c.getName())) {
-	//					capitalFactorCurves.put(c, curves.get(c.getName()));
-	//				}
-	//			}
-	//		} catch (NumberFormatException e) {
-	//			logger.error("A required number could not be parsed from " + capitalAdjustmentsCSV
-	//					+ ". Make "
-	//					+ "sure the CSV files contains columns " + modelData.services.names());
-	//			throw e;
-	//		}
-	//	}
-	
+
 
 	//	/**
 	//	 * 	
@@ -238,8 +215,8 @@ public class NeighbourhoodNetworkInstitution extends AbstractInstitution {
 	//		c.setEffectiveCapitals(adjusted);
 	//
 	//	}
-	
-	
+
+
 	/**
 	 * @see java.lang.Object#toString()
 	 */
